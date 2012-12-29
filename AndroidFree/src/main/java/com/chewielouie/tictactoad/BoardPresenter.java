f3e899baa8a172ae1d182099e5fea0e6cc5f4c70
Julian Churchill <chewielouie03@gmail.com>
2012-12-29 22:37:40 +0000

    Implemented presenter render method

--- AndroidFree/src/main/java/com/chewielouie/tictactoad/BoardPresenter.java
+++ AndroidFree/src/main/java/com/chewielouie/tictactoad/BoardPresenter.java
@@ -1,9 +1,15 @@
 package com.chewielouie.tictactoad;
 
 class BoardPresenter {
+    private BoardModel model;
+    private BoardView view;
+
     public BoardPresenter( BoardModel m, BoardView v ) {
+        this.model = m;
+        this.view = v;
     }
 
     public void render() {
+        view.displayBoard( model.retrieveBoard() );
     }
 }

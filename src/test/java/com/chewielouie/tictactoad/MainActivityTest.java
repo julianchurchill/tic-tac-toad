6b053182198675aa3038c9ea226cf3e9f65b0e9d
Julian Churchill <chewielouie03@gmail.com>
2012-12-30 18:45:27 +0000

    Added ant rules and example test for android unit testing with junit

---
+++ src/test/java/com/chewielouie/tictactoad/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.chewielouie.tictactoad;
+
+import static org.junit.Assert.*;
+
+//import cmo.xtremelabs.roboelectric.RoboelectricTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class MainActivityTest {
+    @Test
+    public void testFail() {
+        fail("Fail!");
+    }
+}
+

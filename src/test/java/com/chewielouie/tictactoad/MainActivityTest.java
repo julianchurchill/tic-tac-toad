811c25bf259936cadc379eac9c1b0c7b580f4e29
Julian Churchill <chewielouie03@gmail.com>
2012-12-30 20:09:28 +0000

    Added robolectric lib for andriod unit testing and updated classpaths for unit test rules to build

--- src/test/java/com/chewielouie/tictactoad/MainActivityTest.java
+++ src/test/java/com/chewielouie/tictactoad/MainActivityTest.java
@@ -2,10 +2,11 @@ package com.chewielouie.tictactoad;
 
 import static org.junit.Assert.*;
 
-//import cmo.xtremelabs.roboelectric.RoboelectricTestRunner;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+@RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
     @Test
     public void testReferencingAndroidClass() {

fb12720ff17011544bf6aa324c928698af4d1342
Julian Churchill <chewielouie03@gmail.com>
2013-01-06 21:03:14 +0000

    Basic NormalBoard implemented with width, height, set and get content

---
+++ AndroidFree/src/main/java/com/chewielouie/tictactoad/NormalBoard.java
@@ -0,0 +1,34 @@
+package com.chewielouie.tictactoad;
+
+import com.chewielouie.tictactoad.Board;
+
+//public class NormalBoard implements Board {
+public class NormalBoard {
+    private final int WIDTH = 3;
+    private final int HEIGHT = 3;
+    private Board.Piece piece = Board.Piece.None;
+    private Board.Piece[][] pieces;
+
+    public NormalBoard() {
+        pieces = new Board.Piece[WIDTH][HEIGHT];
+        for( int x = 0; x < WIDTH; ++x )
+            for( int y = 0; y < HEIGHT; ++y )
+                pieces[x][y] = Board.Piece.None;
+    }
+
+    public int width() {
+        return WIDTH;
+    }
+
+    public int height() {
+        return HEIGHT;
+    }
+
+    public void setContentAt( Coord c, Board.Piece p ) {
+        pieces[c.x()][c.y()] = p;
+    }
+
+    public Board.Piece getContentAt( Coord c ) {
+        return pieces[c.x()][c.y()];
+    }
+}
---
+++ AndroidFree/src/test/java/com/chewielouie/tictactoad/NormalBoardTests.java
@@ -0,0 +1,68 @@
+package com.chewielouie.tictactoad;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NormalBoardTests {
+
+    @Test
+    public void should_be_3_by_3() {
+        final NormalBoard board = new NormalBoard();
+
+        assertEquals( 3, board.width() );
+        assertEquals( 3, board.height() );
+    }
+
+    @Test
+    public void should_be_empty_on_construction() {
+        final NormalBoard board = new NormalBoard();
+
+        for( int x = 0; x < board.width(); ++x )
+            for( int y = 0; y < board.height(); ++y )
+                assertEquals( Board.Piece.None,
+                              board.getContentAt( new Coord( x, y ) ) );
+
+        //BoardIterator iter = board.start();
+        //for( ; iter != board.end(); ++iter )
+            //assertEquals( Board.Piece.None, iter.piece() );
+    }
+
+    @Test
+    public void should_return_piece_that_was_set() {
+        final NormalBoard board = new NormalBoard();
+
+        board.setContentAt( new Coord( 1, 0 ), Board.Piece.Nought );
+
+        assertEquals( Board.Piece.Nought, board.getContentAt( new Coord( 1, 0 ) ) );
+    }
+
+    @Test
+    public void should_not_change_any_other_piece_when_one_is_set() {
+        final NormalBoard board = new NormalBoard();
+
+        final int test_x = 1;
+        final int test_y = 0;
+        board.setContentAt( new Coord( test_x, test_y ), Board.Piece.Nought );
+
+        final NormalBoard emptyBoard = new NormalBoard();
+        for( int x = 0; x < board.width(); ++x )
+            for( int y = 0; y < board.height(); ++y )
+                if( x != test_x && y != test_y )
+                    assertEquals( emptyBoard.getContentAt( new Coord( x, y ) ),
+                                  board.getContentAt( new Coord( x, y ) ) );
+
+        //final Coord test_coord = new Coord( 1, 0 );
+        //BoardIterator iter = board.start();
+        //for( ; iter != board.end(); ++iter )
+            //if( iter.coord() != test_coord )
+                //assertEquals( emptyBoard.getContentAt( iter.coord() ),
+                              //iter.piece() );
+    }
+
+    //public void is_a_value_object() {
+    // Contract test?
+    //public void should_throw_an_exception_when_asked_for_a_piece_outside_of_the_board() {
+}
+
+

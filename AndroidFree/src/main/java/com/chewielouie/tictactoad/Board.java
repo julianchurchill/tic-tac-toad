a4ecfb2df0ddb0bd21819ca796ad37effbf98813
Julian Churchill <chewielouie03@gmail.com>
2013-01-11 22:41:15 +0000

    Added width and height

--- AndroidFree/src/main/java/com/chewielouie/tictactoad/Board.java
+++ AndroidFree/src/main/java/com/chewielouie/tictactoad/Board.java
@@ -3,5 +3,7 @@ package com.chewielouie.tictactoad;
 public interface Board {
     public enum Piece { None, Nought, Cross };
 
+    public int width();
+    public int height();
     public Piece getContentAt( Coord c );
 }

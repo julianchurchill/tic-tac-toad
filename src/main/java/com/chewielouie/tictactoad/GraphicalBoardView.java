d56105ac5f271760a0a88b375f3d81f6ec8e558f
Julian Churchill <chewielouie03@gmail.com>
2013-01-13 20:52:46 +0000

    Added drawing of green frog

--- src/main/java/com/chewielouie/tictactoad/GraphicalBoardView.java
+++ src/main/java/com/chewielouie/tictactoad/GraphicalBoardView.java
@@ -4,6 +4,8 @@ import com.chewielouie.tictactoad.OpenGridShape;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.util.AttributeSet;
@@ -12,10 +14,13 @@ import android.widget.ImageView;
 public class GraphicalBoardView extends ImageView {
     private OpenGridShape grid = new OpenGridShape( 3, 3 );
     private ShapeDrawable lilyPad = new ShapeDrawable();
+    private BitmapDrawable greenFrog = null;
     private Board board = new NullBoard();
+    private Context context;
 
     public GraphicalBoardView( Context context, AttributeSet attrs ) {
         super( context, attrs );
+        this.context = context;
         createDrawableForEmptyBoardPoint();
     }
 
@@ -41,10 +46,36 @@ public class GraphicalBoardView extends ImageView {
 
     private void drawBoardContent( Canvas c ) {
         BoardIterator iterator = board.iterator();
-        for( ; iterator.hasNext(); iterator.next() ) {
-            lilyPad.setBounds( grid.cellBounds( iterator.coord() ) );
-            lilyPad.draw( c );
+        for( ; iterator.hasNext(); iterator.next() )
+            new BoardCellPainter( c,
+                                  grid.cellBounds( iterator.coord() ) );
+    }
+
+    class BoardCellPainter {
+        Canvas canvas;
+        Rect bounds;
+
+        public BoardCellPainter( Canvas c, Rect b ) {
+            canvas = c;
+            bounds = b;
+            drawLilyPad();
+            drawGreenFrog();
         }
+
+        private void drawLilyPad() {
+            lilyPad.setBounds( bounds );
+            lilyPad.draw( canvas );
+        }
+
+        private void drawGreenFrog() {
+            canvas.drawBitmap( greenFrog().getBitmap(),
+                    bounds.left, bounds.top, null );
+        }
+    }
+
+    private BitmapDrawable greenFrog() {
+        return (BitmapDrawable)context.getResources().
+                    getDrawable( R.drawable.green_frog );
     }
 
     public OpenGridShape grid() {
